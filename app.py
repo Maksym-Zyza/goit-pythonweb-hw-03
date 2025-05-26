fa9ed9cf3cf0c55fbcd032d8fb36d80e66c1eb00
Maksym.Zyza <mmziza88@gmail.com>
2025-05-26 21:18:28 +0300

    feat: improve project file structure

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import mimetypes
 import pathlib
 import urllib.parse
-from render_messages import render_messages
+from render_template import render_template
 from save_to_storage import save_to_storage
 
 
@@ -14,9 +14,8 @@ class SimpleHttpRequestHandler(BaseHTTPRequestHandler):
             self.send_html_file("index.html")
         elif pr_url.path == "/message.html":
             self.send_html_file("message.html")
-        elif pr_url.path == "/read":
-            render_messages()
-            self.send_html_file("messages.html")
+        elif pr_url.path == "/read.html":
+            self.send_html_file("read.html")
         else:
             if pathlib.Path().joinpath(pr_url.path[1:]).exists():
                 self.send_static()
@@ -24,16 +23,18 @@ class SimpleHttpRequestHandler(BaseHTTPRequestHandler):
                 self.send_html_file("error.html", 404)
 
     def do_POST(self):
-      data = self.rfile.read(int(self.headers['Content-Length']))
-      data_parse = urllib.parse.unquote_plus(data.decode())
-      data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
-      save_to_storage(data_dict)
-      self.send_response(302)
-      self.send_header('Location', '/')
-      self.end_headers()
-
+        data = self.rfile.read(int(self.headers["Content-Length"]))
+        data_parse = urllib.parse.unquote_plus(data.decode())
+        data_dict = {
+            key: value for key, value in [el.split("=") for el in data_parse.split("&")]
+        }
+        save_to_storage(data_dict)
+        self.send_response(302)
+        self.send_header("Location", "/")
+        self.end_headers()
 
     def send_html_file(self, filename, status=200):
+        render_template(filename)
         self.send_response(status)
         self.send_header("Content-type", "text/html")
         self.end_headers()
--- render_template.py
+++ render_template.py
@@ -3,18 +3,18 @@ from pathlib import Path
 import json
 
 
-def render_messages():
+def render_template(filename):
     env = Environment(loader=FileSystemLoader("templates"))
-    template = env.get_template("messages.html")
+    template = env.get_template(filename)
 
     storage_path = Path("storage/data.json")
-    if storage_path.exists():
+    if filename == "read.html" and storage_path.exists():
         with open(storage_path, "r", encoding="utf-8") as f:
             messages = json.load(f)
     else:
         messages = {}
 
-    output = template.render(messages=messages)
+    output = template.render(messages=messages, active_page=filename)
 
-    with open("messages.html", "w", encoding="utf-8") as fh:
+    with open(filename, "w", encoding="utf-8") as fh:
         fh.write(output)
